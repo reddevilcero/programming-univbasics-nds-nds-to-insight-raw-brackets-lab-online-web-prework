c40ea27b978554bf6ee08355675797106e3a51c7
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2019-12-20 16:30:24 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require "pp"
+
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
@@ -12,11 +14,12 @@ def directors_totals(nds)
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
+  for director in directors_database
+    result[director[:name]] = 0
+    director[:movies].each { |movie|
+      result[director[:name]] += movie[:worldwide_gross]
+    }
+  end
   # Be sure to return the result at the end!
-  nil
+  return result
 end
